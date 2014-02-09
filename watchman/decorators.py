046012c06b15cfd95b4c5c1575010d3aa1b319c4
Michael Warkentin <mwarkentin@gmail.com>
2014-02-09 12:18:10 -0500

    Add `required_token` decorator tests

--- watchman/decorators.py
+++ watchman/decorators.py
@@ -4,7 +4,7 @@ from django.views.decorators.csrf import csrf_exempt
 
 from functools import wraps
 
-from watchman.settings import WATCHMAN_TOKEN, WATCHMAN_TOKEN_NAME
+from watchman import settings as watchman_settings
 
 
 def token_required(view_func):
@@ -13,7 +13,9 @@ def token_required(view_func):
     @csrf_exempt
     @wraps(view_func)
     def _wrapped_view(request, *args, **kwargs):
-        if WATCHMAN_TOKEN is None or WATCHMAN_TOKEN == request.GET.get(WATCHMAN_TOKEN_NAME):
+        watchman_token = watchman_settings.WATCHMAN_TOKEN
+        watchman_token_name = watchman_settings.WATCHMAN_TOKEN_NAME
+        if watchman_token is None or watchman_token == request.GET.get(watchman_token_name):
             return view_func(request, *args, **kwargs)
 
         return HttpResponseForbidden()
