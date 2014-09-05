fa00a3c50800cbbd7a9f36aab2c185ffc1b13b34
Michael Warkentin <mwarkentin@gmail.com>
2014-09-05 11:18:38 -0400

    Change json decoding based on python version

--- tests/test_views.py
+++ tests/test_views.py
@@ -13,11 +13,14 @@ from __future__ import unicode_literals
 import django
 import json
 import unittest
+import sys
 from django.conf import settings
 from django.test.client import RequestFactory
 from mock import patch
 
 from watchman import checks, views
+
+PYTHON_VERSION = sys.version_info[0]
 
 if django.VERSION >= (1, 7):
     # Initialize Django
@@ -45,7 +48,10 @@ class TestWatchman(unittest.TestCase):
         patched_check_databases.return_value = []
         request = RequestFactory().get('/')
         response = views.status(request)
-        content = json.loads(response.content)
+        if PYTHON_VERSION == 2:
+            content = json.loads(response.content)
+        else:
+            content = json.loads(response.content.decode('utf-8'))
         self.assertItemsEqual(expected_checks, content.keys())
 
     def test_check_database_handles_exception(self):
@@ -66,7 +72,10 @@ class TestWatchman(unittest.TestCase):
             'check': 'watchman.checks.databases_status',
         })
         response = views.status(request)
-        content = json.loads(response.content)
+        if PYTHON_VERSION == 2:
+            content = json.loads(response.content)
+        else:
+            content = json.loads(response.content.decode('utf-8'))
         self.assertEqual(response.status_code, 200)
         self.assertItemsEqual({'databases': []}, content)
 
@@ -77,7 +86,10 @@ class TestWatchman(unittest.TestCase):
             'check': '',
         })
         response = views.status(request)
-        content = json.loads(response.content)
+        if PYTHON_VERSION == 2:
+            content = json.loads(response.content)
+        else:
+            content = json.loads(response.content.decode('utf-8'))
         self.assertEqual(response.status_code, 404)
         self.assertItemsEqual({'message': 'No checks found', 'error': 404},
                               content)
