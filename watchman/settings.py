037a9e7d5a6f91e2391a42c403ac5a8e50521085
Michael Warkentin <mwarkentin@gmail.com>
2014-12-19 19:39:30 -0500

    Add `storage_status` check
    * Writes, checks size, reads, and then deletes a file
    * Add `storage_status` check to default checks
    * Fixes #7

--- watchman/settings.py
+++ watchman/settings.py
@@ -7,6 +7,7 @@ DEFAULT_CHECKS = (
     'watchman.checks.caches_status',
     'watchman.checks.databases_status',
     'watchman.checks.email_status',
+    'watchman.checks.storage_status',
 )
 
 WATCHMAN_CHECKS = getattr(settings, 'WATCHMAN_CHECKS', DEFAULT_CHECKS)
