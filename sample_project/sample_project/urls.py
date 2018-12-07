f8b1bee27ceb055430fb09de52e177a4c346072e
Jamie Hewland <jhewland@gmail.com>
2018-12-07 12:38:53 +0200

    Regenerate sample project with latest Django LTS (1.11)

--- sample_project/sample_project/urls.py
+++ sample_project/sample_project/urls.py
@@ -1,10 +1,25 @@
+"""sample_project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
 from django.conf.urls import include, url
 from django.contrib import admin
 import watchman.views
 
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
     url(r'^watchman/', include('watchman.urls')),
     url(r'^watchman/bare/', watchman.views.bare_status, name='bare_status'),
 ]
