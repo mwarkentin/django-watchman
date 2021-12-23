e747401d7ba7602a7e5f2c3c6c5b394bdb28ee8d
Michael Warkentin <mwarkentin@hey.com>
2021-12-23 10:29:40 -0500

    add black lint workflow (#178)
    * add black lint workflow
    
    * black .
    
    * add pre-commit for black/flake8
    
    * configure black and flake8
    
    * add make fmt and clean up makefile
    
    * use basic flake8 config from black docs
    
    fix unused imports
    ignore unused import in manage.py as its used to trigger an ImportError
    
    * add isort to pre-commit
    
    * run isort on all .py files
    
    * sort requirements-test.txt
    
    * add isort github action
    
    * master -> main branch refs

--- tests/test_management.py
+++ tests/test_management.py
@@ -9,38 +9,41 @@ Tests for `django-watchman` management commands.
 
 from __future__ import unicode_literals
 
-from io import StringIO
 import unittest
+from io import StringIO
 
 from django.core.management import call_command
 
 
 class TestWatchman(unittest.TestCase):
-
     def test_successful_management_command_outputs_nothing(self):
         out = StringIO()
-        call_command('watchman', stdout=out)
-        self.assertEqual('', out.getvalue())
+        call_command("watchman", stdout=out)
+        self.assertEqual("", out.getvalue())
 
     def test_successful_management_command_outputs_check_status_with_verbosity_2(self):
         out = StringIO()
-        call_command('watchman', stdout=out, verbosity='2')
-        self.assertIn('caches', out.getvalue())
+        call_command("watchman", stdout=out, verbosity="2")
+        self.assertIn("caches", out.getvalue())
 
     def test_successful_management_command_outputs_check_status_with_verbosity_3(self):
         out = StringIO()
-        call_command('watchman', stdout=out, verbosity='3')
-        self.assertIn('caches', out.getvalue())
+        call_command("watchman", stdout=out, verbosity="3")
+        self.assertIn("caches", out.getvalue())
 
     def test_successful_management_command_supports_check_list(self):
         out = StringIO()
-        call_command('watchman', stdout=out, checks='watchman.checks.caches', verbosity='3')
-        self.assertIn('caches', out.getvalue())
-        self.assertNotIn('databases', out.getvalue())
+        call_command(
+            "watchman", stdout=out, checks="watchman.checks.caches", verbosity="3"
+        )
+        self.assertIn("caches", out.getvalue())
+        self.assertNotIn("databases", out.getvalue())
 
     def test_successful_management_command_supports_skip_list(self):
         out = StringIO()
-        call_command('watchman', stdout=out, skips='watchman.checks.email', verbosity='3')
-        self.assertIn('caches', out.getvalue())
-        self.assertIn('databases', out.getvalue())
-        self.assertNotIn('email', out.getvalue())
+        call_command(
+            "watchman", stdout=out, skips="watchman.checks.email", verbosity="3"
+        )
+        self.assertIn("caches", out.getvalue())
+        self.assertIn("databases", out.getvalue())
+        self.assertNotIn("email", out.getvalue())
