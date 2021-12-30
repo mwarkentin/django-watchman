ae7eb460331bfe8da4cd7e7a316a40953cbfbe4b
cmoyano <cristianmoyano.mza@gmail.com>
2021-12-30 10:15:07 -0300

    fix tests

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -14,6 +14,7 @@ import unittest
 from unittest import mock
 
 from django.test.client import Client
+from django.urls import reverse
 from unittest.mock import MagicMock, patch
 
 from watchman import settings as watchman_settings
@@ -24,6 +25,20 @@ class FakeException(Exception):
     def __init__(self, *args, **kwargs):
         super(self.__class__, self).__init__(*args, **kwargs)
 
+
+class TestDBConnection(unittest.TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    @patch('django.db.connections')
+    def test_cursor_is_called(self, mock_connections):
+        cursor_mock = MagicMock()
+        mock_connections['default'].cursor().__enter__.return_value = cursor_mock
+        data = {
+            'watchman-token': 't1',
+        }
+        self.client.get(reverse('status'), data)
+        cursor_mock.execute.assert_called_once_with('SELECT 1')
 
 class TestWatchmanMultiTokens(unittest.TestCase):
     def setUp(self):
@@ -37,17 +52,6 @@ class TestWatchmanMultiTokens(unittest.TestCase):
         }
         response = self.client.get(reverse("status"), data)
         self.assertEqual(response.status_code, 200)
-
-    @patch('django.db.connections')
-    def test_cursor_is_called(self, mock_connections):
-        cursor_mock = MagicMock()
-        mock_connections['default'].cursor().__enter__.return_value = cursor_mock
-        data = {
-            'watchman-token': 't1',
-        }
-        self.client.get(reverse('status'), data)
-        cursor_mock.execute.assert_called_once_with('SELECT 1')
-
 
     def test_200_ok_if_matching_second_token_in_list(self):
         data = {
