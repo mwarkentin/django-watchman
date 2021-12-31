29f34f162317ab3ead58ce3dcae89018ceabf05b
cmoyano <cristianmoyano.mza@gmail.com>
2021-12-31 14:12:36 -0300

    undo changes

--- tests/test_views.py
+++ tests/test_views.py
@@ -80,14 +80,16 @@ class TestWatchman(unittest.TestCase):
     def test_check_database_handles_exception(self):
         response = checks._check_database("foo")
         self.assertFalse(response["foo"]["ok"])
-        self.assertEqual(response["foo"]["error"], "The connection foo doesn't exist")
+        self.assertEqual(
+            response["foo"]["error"], "The connection 'foo' doesn't exist."
+        )
 
     def test_check_cache_handles_exception(self):
         response = checks._check_cache("foo")
         self.assertFalse(response["foo"]["ok"])
         self.assertIn(
             response["foo"]["error"],
-            "Could not find config for 'foo' in settings.CACHES",
+            "The connection 'foo' doesn't exist.",
         )
 
     def test_response_skipped_checks(self):
